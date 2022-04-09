61840b8ca4c5eedf7928aa5d5ae8f510a32a0f63
NikkelM <nikkel.mollenhauer@student.hpi.de>
2022-04-09 13:29:58 +0200

    Better scroll

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # My portfolio Website
 
-This repository contains the source code for my portfolio website over at [localhost :P](http://localhost:3000/).
+This repository contains the source code for my portfolio website over at [localhost](http://localhost:3000/) :P
  
--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import './style.css';
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // Setup
 
@@ -14,18 +13,18 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
-camera.position.setX(-3);
+camera.position.z = 30;
+camera.position.x = 0;
 
 renderer.render(scene, camera);
 
-// Torus
-
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
-const torus = new THREE.Mesh(geometry, material);
-
-scene.add(torus);
+window.addEventListener('resize', onWindowResize, false)
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+}
 
 // Lights
 
@@ -34,14 +33,6 @@ pointLight.position.set(5, 5, 5);
 
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
-
-// Helpers
-
-// const lightHelper = new THREE.PointLightHelper(pointLight)
-// const gridHelper = new THREE.GridHelper(200, 50);
-// scene.add(lightHelper, gridHelper)
-
-// const controls = new OrbitControls(camera, renderer.domElement);
 
 function addStar() {
   const geometry = new THREE.SphereGeometry(0.25, 24, 24);
@@ -65,64 +56,73 @@ scene.background = spaceTexture;
 
 // Avatar
 
-const jeffTexture = new THREE.TextureLoader().load('jeff.png');
+const profileTexture = new THREE.TextureLoader().load('images/profile.jpg');
 
-const jeff = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: jeffTexture }));
+const profile = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: profileTexture }));
+profile.position.z = -5;
+profile.position.x = 3;
 
-scene.add(jeff);
+scene.add(profile);
 
-// Moon
+// Animation template from https://sbcode.net/threejs/animate-on-scroll/
 
-const moonTexture = new THREE.TextureLoader().load('moon.jpg');
-const normalTexture = new THREE.TextureLoader().load('normal.jpg');
+// Animation Helpers
 
-const moon = new THREE.Mesh(
-  new THREE.SphereGeometry(3, 32, 32),
-  new THREE.MeshStandardMaterial({
-    map: moonTexture,
-    normalMap: normalTexture,
-  })
-);
-
-scene.add(moon);
-
-moon.position.z = 30;
-moon.position.setX(-10);
-
-jeff.position.z = -5;
-jeff.position.x = 2;
-
-// Scroll Animation
-
-function moveCamera() {
-  const t = document.body.getBoundingClientRect().top;
-//   moon.rotation.x += 0.05;
-//   moon.rotation.y += 0.075;
-//   moon.rotation.z += 0.05;
-
-//   jeff.rotation.y += 0.01;
-//   jeff.rotation.z += 0.01;
-
-  camera.position.z = t * -0.01;
-//   camera.position.x = t * -0.0002;
-//   camera.rotation.y = t * -0.0002;
+function lerp(start, end, a) {
+  return (1 - a) * start + a * end;
 }
 
-document.body.onscroll = moveCamera;
-moveCamera();
+// Used to fit the lerps to start and end at specific scrolling percentages
+function scalePercent(start, end) {
+  return (scrollPercent - start) / (end - start)
+}
+
+// Animation scripts that will each be run whenever the user scrolls
+
+const animationScripts = []
+
+//add an animation that moves the camera backwards throughout the whole scroll progress
+animationScripts.push({
+  start: 0,
+  end: 101,
+  func: () => {
+      camera.position.z = lerp(0, 20, scalePercent(0, 100))
+  },
+});
+
+//add an animation that rotates the profile cube throughout the whole scroll process
+animationScripts.push({
+  start: 0,
+  end: 101,
+  func: () => {
+      profile.rotation.y = lerp(-0.4, 5, scalePercent(0, 100))
+  },
+});
+
+// How far down the rabbit hole are we???
+let scrollPercent = 0
+
+document.body.onscroll = () => {
+    //calculate the current scroll progress as a percentage
+    scrollPercent = ((document.documentElement.scrollTop || document.body.scrollTop) / 
+      ((document.documentElement.scrollHeight || document.body.scrollHeight) - document.documentElement.clientHeight)) * 100;
+};
 
 // Animation Loop
+
+// Loop through all scripts and run each of them
+function playScrollAnimations() {
+  animationScripts.forEach((animation) => {
+      if (scrollPercent >= animation.start && scrollPercent < animation.end) {
+        animation.func();
+      }
+  })
+};
 
 function animate() {
   requestAnimationFrame(animate);
 
-//   torus.rotation.x += 0.01;
-//   torus.rotation.y += 0.005;
-//   torus.rotation.z += 0.01;
-
-//   moon.rotation.x += 0.005;
-
-  // controls.update();
+  playScrollAnimations()
 
   renderer.render(scene, camera);
 }
