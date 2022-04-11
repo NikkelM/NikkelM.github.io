09e900c5857b17912070c55fc629f408a91b89a3
NikkelM <nikkel.mollenhauer@student.hpi.de>
2022-04-11 21:32:57 +0200

    Updates

--- index.js
+++ index.js
@@ -1,94 +1,120 @@
 import * as THREE from 'https://unpkg.com/three@0.139.2/build/three.module.js';
 
+let camera, scene, renderer, textureLoader;
+let skybox, starGroup, avatarCube;
 
-const textureLoader = new THREE.TextureLoader();
+let scrollPercent = 0;
+const animationScripts = [];
+let starLightness = 0;
 
-const scene = new THREE.Scene();
+init();
+animate();
 
-scene.fog = new THREE.FogExp2(0x000000, 0.0002);
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 30000);
+function init() {
+	// reset the scroll when the page is reloaded to make sure our animations aren't getting messed up 
+	if (history.scrollRestoration) {
+		history.scrollRestoration = 'manual';
+	} else {
+		window.onbeforeunload = function () {
+			window.scrollTo(0, 0);
+		}
+	}
 
-const renderer = new THREE.WebGLRenderer({
-	antialias: true,
-	alpha: true,
-	canvas: document.querySelector('#bg'),
-});
+	camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 30000);
 
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
+	scene = new THREE.Scene();	
+	scene.fog = new THREE.FogExp2(0x000000, 0.02);
 
-renderer.render(scene, camera);
+	// loading screen init
+	const loadingManager = new THREE.LoadingManager(() => {
+		const loadingScreen = document.getElementById('loading-screen');
+		loadingScreen.classList.add('fade-out');
+		loadingScreen.addEventListener('transitionend', onLoadingScreenTransitionEnd);
+	});
 
-window.addEventListener('resize', onWindowResize, false)
+	textureLoader = new THREE.TextureLoader(loadingManager);
+
+
+	// Lights
+
+	// const pointLight = new THREE.PointLight(0xffffff);
+	// pointLight.position.set(5, 5, 5);
+
+	// const ambientLight = new THREE.AmbientLight(0xffffff);
+	// scene.add(pointLight, ambientLight);
+
+	// add the various models
+	initSkybox();
+	initStars();
+	initAvatarCube();
+
+	renderer = new THREE.WebGLRenderer({
+		antialias: true,
+		alpha: true,
+		canvas: document.querySelector('#bg'),
+	});
+	
+	renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setSize(window.innerWidth, window.innerHeight);
+
+	window.addEventListener('resize', onWindowResize, false);
+}
+
 function onWindowResize() {
-		camera.aspect = window.innerWidth / window.innerHeight;
-		camera.updateProjectionMatrix();
-		renderer.setSize(window.innerWidth, window.innerHeight);
-		renderer.render(scene, camera);
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.render(scene, camera);
 }
-
-// Lights
-
-// const pointLight = new THREE.PointLight(0xffffff);
-// pointLight.position.set(5, 5, 5);
-
-// const ambientLight = new THREE.AmbientLight(0xffffff);
-// scene.add(pointLight, ambientLight);
-
-
-// Stars
-
-const starGroup = new THREE.Group();
-let starGeometry = new THREE.SphereGeometry(0.05);
-
-const numStars = 500;
-// we need some variety of materials for the blinking to appear more random
-const numStarMaterials = numStars/5;
-const starMaterials = [];
-
-for(let i=0; i<numStarMaterials; i++) {
-	starMaterials.push(new THREE.MeshBasicMaterial());
-}
-
-for (let i = 0; i < numStars; i++) {
-  let star = new THREE.Mesh(starGeometry, starMaterials[i%numStarMaterials]);
-
-	// Get a positional vector for the star, it should be between 30 and 40 units from the camera
-	let vec = new THREE.Vector3(THREE.MathUtils.randFloatSpread(70), THREE.MathUtils.randFloatSpread(70), THREE.MathUtils.randFloatSpread(70)).clampLength(30, 40)
-  star.position.set(vec.x, vec.y, vec.z);
-
-	star.material.transparent = true;
-  starGroup.add(star);
-}
-scene.add(starGroup);
 
 // Skybox
+function initSkybox() {
+	const materialArray = ['right1', 'left2', 'top3', 'bottom4', 'front5', 'back6'].map(image => {
+		let texture = textureLoader.load('static/textures/skybox/' + image + '.jpg');
+		return new THREE.MeshBasicMaterial({ map: texture, side: THREE.BackSide });
+	});
 
-const materialArray = ['right1.jpg', 'left2.jpg', 'top3.jpg', 'bottom4.jpg',
-	'front5.jpg', 'back6.jpg'].map(image => {
-	let texture = textureLoader.load('static/textures/skybox/' + image);
-	return new THREE.MeshBasicMaterial({ map: texture, side: THREE.BackSide });
-});
+	skybox = new THREE.Mesh(new THREE.BoxGeometry(100, 100, 100), materialArray);
+	scene.add(skybox);
+}
 
-const skybox = new THREE.Mesh(new THREE.BoxGeometry(10000, 10000, 10000), materialArray);
+// Stars
+function initStars() {
+	starGroup = new THREE.Group();
+	let starGeometry = new THREE.SphereGeometry(0.05);
 
-scene.add(skybox);
+	const numStars = 500;
+	// we need some variety of materials for the blinking to appear more random
+	const numStarMaterials = numStars/5;
+	const starMaterials = [];
+
+	for(let i=0; i<numStarMaterials; i++) {
+		starMaterials.push(new THREE.MeshBasicMaterial());
+	}
+
+	for (let i = 0; i < numStars; i++) {
+		let star = new THREE.Mesh(starGeometry, starMaterials[i%numStarMaterials]);
+
+		// Get a positional vector for the star, it should be between 30 and 40 units from the camera
+		let vec = new THREE.Vector3(THREE.MathUtils.randFloatSpread(70), THREE.MathUtils.randFloatSpread(70), THREE.MathUtils.randFloatSpread(70)).clampLength(30, 40)
+		star.position.set(vec.x, vec.y, vec.z);
+
+		star.material.transparent = true;
+		starGroup.add(star);
+	}
+	scene.add(starGroup);
+}
 
 // Avatar Cube
+function initAvatarCube() {
+	const avatarCubeTexture = textureLoader.load('static/textures/avatarCube.jpg');
 
-const profileTexture = textureLoader.load('static/textures/profile.jpg');
-
-const profile = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: profileTexture }));
-const profileStartPositionX = 3;
-const profileStartPositionY = 0;
-const profileStartPositionZ = -5;
-const profileStartRotationX = 0;
-const profileStartRotationY = -0.4;
-const profileStartRotationZ = 0;
-profile.position.set(profileStartPositionX, profileStartPositionY, profileStartPositionZ)
-profile.rotation.set(profileStartRotationX, profileStartRotationY, profileStartRotationZ)
-
-scene.add(profile);
+	avatarCube = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: avatarCubeTexture }));
+	avatarCube.position.set(3, 0, -5)
+	avatarCube.rotation.set(0, -0.4, 0)
+	
+	scene.add(avatarCube);
+}
 
 // Globe
 
@@ -107,10 +133,9 @@ scene.add(profile);
 
 // scene.add(globe);
 
+
 // Animation template from https://sbcode.net/threejs/animate-on-scroll/
-
 // Animation Helpers
-
 function lerp(start, end, a) {
 	return (1 - a) * start + a * end;
 }
@@ -120,23 +145,17 @@ function scalePercent(start, end) {
 	return (scrollPercent - start) / (end - start);
 }
 
-
 // How much has the user scrolled yet?
-let scrollPercent = 0;
-
 document.body.onscroll = () => {
-		//calculate the current scroll progress as a percentage
-		scrollPercent = ((document.documentElement.scrollTop || document.body.scrollTop) / 
-			((document.documentElement.scrollHeight || document.body.scrollHeight) - document.documentElement.clientHeight)) * 100;
+	//calculate the current scroll progress as a percentage
+	scrollPercent = ((document.documentElement.scrollTop || document.body.scrollTop) / 
+		((document.documentElement.scrollHeight || document.body.scrollHeight) - document.documentElement.clientHeight)) * 100;
 }
 
 /////////////////////////
 // Animation scripts that will each be run whenever the user scrolls
 
-const animationScripts = [];
-
-// These are the continuous animations which play no matter the scroll percentage, and independent of
-let lightness = 0;
+// These are the continuous animations which play no matter the scroll percentage
 animationScripts.push({
 	start: 0,
 	end: 101,
@@ -145,8 +164,8 @@ animationScripts.push({
 		skybox.rotation.x += 0.001;
 		skybox.rotation.y -= 0.0005;
 
-		// profile
-		profile.rotation.y -= 0.005;
+		// avatarCube
+		avatarCube.rotation.y -= 0.005;
 
 		// globe
 		// const quaternion = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0).normalize(), 0.0001);
@@ -156,25 +175,25 @@ animationScripts.push({
 		starGroup.rotation.x += 0.001;
 		starGroup.rotation.y -= 0.0005;
 		starGroup.children.forEach((star) => {
-			lightness > 1 ? lightness = 0 : lightness += 0.005;
-			star.material.opacity = lightness;
+			starLightness > 1 ? starLightness = 0 : starLightness += 0.005;
+			star.material.opacity = starLightness;
 		});
 	}
 });
 
-// Add an animation that rotates the profile cube throughout the whole scroll process
+// Add an animation that rotates the avatarCube cube throughout the whole scroll process
 animationScripts.push({
 	start: 0,
 	end: 20,
 	func: () => {
-			profile.rotation.x = lerp(profileStartRotationX, 2, scalePercent(0, 20));
-			profile.rotation.z = lerp(profileStartRotationZ, -2, scalePercent(0, 20));
-			profile.position.x = lerp(profileStartPositionX, 10, scalePercent(0, 20));
-			profile.position.y = lerp(profileStartPositionY, 10, scalePercent(0, 20));
+		avatarCube.rotation.x = lerp(0, 2, scalePercent(0, 20));
+		avatarCube.rotation.z = lerp(0, -2, scalePercent(0, 20));
+		avatarCube.position.x = lerp(3, 10, scalePercent(0, 20));
+		avatarCube.position.y = lerp(0, 10, scalePercent(0, 20));
 	}
 });
 
-// Add an animation that moves the globe in after the profile is gone
+// Add an animation that moves the globe in after the avatarCube is gone
 // animationScripts.push({
 // 	start: 5,
 // 	end: 15,
@@ -197,25 +216,14 @@ function playScrollAnimations() {
 	});
 }
 
-
 function animate() {
 	requestAnimationFrame(animate);
 	playScrollAnimations();
-
 	renderer.render(scene, camera);
 }
 
-function init() {
-	// reset the scroll when the page is reloaded to make sure our animations aren't getting messed up 
-	if (history.scrollRestoration) {
-		history.scrollRestoration = 'manual';
-	} else {
-    window.onbeforeunload = function () {
-        window.scrollTo(0, 0);
-    }
-	}
-	// start the animation loop
-	animate()
+// will be called when the loading screen has finished
+function onLoadingScreenTransitionEnd( event ) {
+	document.body.style = "";
+	event.target.remove();
 }
-
-init();
