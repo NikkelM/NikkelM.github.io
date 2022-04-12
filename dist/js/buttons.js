024783602efa302595d4441fef44204f719bdc41
NikkelM <nikkel.mollenhauer@student.hpi.de>
2022-04-12 17:07:37 +0200

    Remove overlay on click outside the form

--- dist/js/buttons.js
+++ dist/js/buttons.js
@@ -1,20 +1,22 @@
 export function initContactForm() {
 	let contactFormLink = document.getElementById("contactLink");
 
-	//Set code to run when the link is clicked
-	// by assigning a function to "onclick"
 	contactFormLink.onclick = function() {
 		overlayContactForm();
 		return false;
 	}
+
+	let contactForm = document.getElementById("contactForm");
+	contactForm.addEventListener('click', function(e) {
+		e.stopPropagation();
+	})
 }
 
 function overlayContactForm() {
-  document.getElementById("contactForm").style.display = "block";
+  document.getElementById("contactFormDiv").onclick = function() {
+		document.getElementById("contactFormDiv").style.display = "none";
+		document.body.style = "";
+	}
+  document.getElementById("contactFormDiv").style.display = "block";
 	document.body.style = "overflow: hidden;";
-}
-
-function removeContactForm() {
-  document.getElementById("contactForm").style.display = "none";
-	document.body.style = "";
 }
