9ae464e292c81a9071000ca5c034c36c86a8d87c
NikkelM <nikkel.mollenhauer@gmx.de>
2023-05-24 20:12:20 +0200

    Updates

---
+++ js/common.js
@@ -0,0 +1,61 @@
+import Cookies from 'js-cookie';
+import './navBar.js';
+
+console.log("Made with ❤️ by @NikkelM");
+console.log("Find the source code at https://github.com/NikkelM/NikkelM.github.io");
+
+init();
+
+function init() {
+	window.onload = function () {
+		initContactForm();
+		checkCookieConsent();
+	}
+}
+
+// ----- Contact form -----
+function initContactForm() {
+	let contactFormLinks = document.querySelectorAll(`[id^="contactLink"]`);
+
+	contactFormLinks.forEach(element => {
+		element.onclick = function() {
+			overlayContactForm();
+			return false;
+		}
+	});
+
+	let contactForm = document.getElementById("contactForm");
+	// stopPropagation to stop the whole contact form from disappearing if the form is clicked
+	contactForm.addEventListener('click', function(e) {
+		e.stopPropagation();
+	})
+}
+
+function overlayContactForm() {
+  document.getElementById("contactFormDiv").onclick = function() {
+		document.getElementById("contactFormDiv").style.display = "none";
+		document.body.style = "";
+	}
+  document.getElementById("contactFormDiv").style.display = "block";
+}
+
+// ----- Cookies -----
+function checkCookieConsent() {
+	const cookieConsent = document.getElementById('cookieConsent');
+	const acceptCookiesButton = document.getElementById('acceptCookiesButton');
+
+	// Check if the user has already accepted cookies
+	if (!Cookies.get('cookieConsent')) {
+		// Show the cookie consent form
+		cookieConsent.style.display = 'block';
+	}
+
+	// Add an event listener to the accept button
+	acceptCookiesButton.addEventListener('click', function () {
+		// Set a cookie to indicate that the user has accepted cookies
+		Cookies.set('cookieConsent', 'true', { expires: 365 });
+
+		// Hide the cookie consent form
+		cookieConsent.style.display = 'none';
+	});
+}
