af833c660d22f7ada807fc32a249b7e583d306eb
Nikkel Mollenhauer <nikkel.mollenhauer@student.hpi.de>
2023-07-25 23:36:18 +0200

    Fixed font preload, overlay

--- src/js/index/index.js
+++ src/js/index/index.js
@@ -3,9 +3,11 @@ import '../common.js';
 init();
 
 function init() {
-	// Fade out the loadingOverlay
-	document.getElementById("loadingOverlay").classList.add("fadeOut");
-	setTimeout(function() {
-		document.getElementById("loadingOverlay").style.display = "none";
-	}, 500);
+	window.onload = function () {
+		// Fade out the loadingOverlay
+		document.getElementById("loadingOverlay").classList.add("fadeOut");
+		setTimeout(function() {
+			document.getElementById("loadingOverlay").style.display = "none";
+		}, 500);
+	}
 }
